a72ed857f9964c3bf993d83d1a9b77b90fd52459
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-03-31 14:03:32 +0300

    question6

--- js/app.js
+++ js/app.js
@@ -1,49 +1,50 @@
 'use strict'
 
 let score = 0;
-let userName = function () {
-    let user = prompt('What\'s your name')
-    return user
-}
+// let userName = function () {
+//     let user = prompt('What\'s your name')
+//     return user
+// }
 
-let validateAnswer = function (question) {
+// let validateAnswer = function (question) {
 
-    let questionContainer = ""
-    // flag to break from the loop
-    let answer = true
-    // to force user to enter y or yes // n or no
-    while (answer) {
+//     let questionContainer = ""
+//     // flag to break from the loop
+//     let answer = true
+//     // to force user to enter y or yes // n or no
+//     while (answer) {
 
-        questionContainer = prompt(question)
-        questionContainer = questionContainer.toLowerCase()
-        if (questionContainer === 'y' || questionContainer === 'yes') {
-            questionContainer = 'yes'
-            answer = false
-            break
-        }
-        else if (questionContainer === 'n' || questionContainer === 'no') {
-            questionContainer = 'no'
-            answer = false
-            break
-        }
-        else {
-            alert('Please Answer with (yes or y),(no or n)')
-        }
-
-
-    }
-    // to return a value then check if it's correct
-    if (questionContainer === 'yes') {
-        return true
-    }
-    else if (questionContainer === 'no') {
-        return false
-    }
+//         questionContainer = prompt(question)
+//         questionContainer = questionContainer.toLowerCase()
+//         if (questionContainer === 'y' || questionContainer === 'yes') {
+//             questionContainer = 'yes'
+//             answer = false
+//             break
+//         }
+//         else if (questionContainer === 'n' || questionContainer === 'no') {
+//             questionContainer = 'no'
+//             answer = false
+//             break
+//         }
+//         else {
+//             alert('Please Answer with (yes or y),(no or n)')
+//         }
 
 
-}
+//     }
+//     // to return a value then check if it's correct
+//     if (questionContainer === 'yes') {
+//         return true
+//     }
+//     else if (questionContainer === 'no') {
+//         return false
+//     }
+
+
+// }
 
 //Guess the number
+function question6(){
 
     let attempts = 4
     let num =3
@@ -74,100 +75,103 @@ let validateAnswer = function (question) {
     if (attempts <= 0) {
         alert(`the right answer was ${num}`)
     }
+}
+// question6();{
 
+// function question7()
+//     let food = ['pizza', 'mansaf', 'makloba', 'egg', 'Koftah', 'kbseh'];
 
-    let food = ['pizza', 'mansaf', 'makloba', 'egg', 'Koftah', 'kbseh'];
-
-    let attempts2 = 6
-    let loop2=true
-    let favFoodUser = prompt('Can you guess what is my fav food ?')
-    for (let i = 0; i < 6; i++) {
+//     let attempts2 = 6
+//     let loop2=true
+//     let favFoodUser = prompt('Can you guess what is my fav food ?')
+//     for (let i = 0; i < 6; i++) {
 
         
-        favFoodUser = favFoodUser.toLowerCase()
-        for (let j = 0; j < 6; j++) {
-            if (favFoodUser == food[j]) {
-                alert('Amazing you are right')
-                score++;
-                alert(`i love these foods ! (possible correct answers were!) ${food}`)
-                loop2=false
-            }
-if (loop2==false)
-{
-    break;
-}
-        }
+//         favFoodUser = favFoodUser.toLowerCase()
+//         for (let j = 0; j < 6; j++) {
+//             if (favFoodUser == food[j]) {
+//                 alert('Amazing you are right')
+//                 score++;
+//                 alert(`i love these foods ! (possible correct answers were!) ${food}`)
+//                 loop2=false
+//             }
+// if (loop2==false)
+// {
+//     break;
+// }
+//         }
 
-        if (loop2==false)
-        {
-            break;
-        }
-
-
-        attempts2--
-        favFoodUser = prompt(`you are wrong you still have ${attempts2}Can you guess one of   my fav foods ?`)
-    }
+//         if (loop2==false)
+//         {
+//             break;
+//         }
 
 
-//Welcome Message
-alert('Hello ' + userName())
+//         attempts2--
+//         favFoodUser = prompt(`you are wrong you still have ${attempts2}Can you guess one of   my fav foods ?`)
+//     }
+// }
+// question7();
+
+// //Welcome Message
+// alert('Hello ' + userName())
 
 
 
 
-// Question one 
-let questionOne = validateAnswer('My name is Bahaa ?')
-if (questionOne) {
-    alert('you  are correct!')
-    score++;
-}
-else {
-    alert('you are wrong!')
-}
+// // Question one 
+// let questionOne = validateAnswer('My name is Bahaa ?')
+// if (questionOne) {
+//     alert('you  are correct!')
+//     score++;
+// }
+// else {
+//     alert('you are wrong!')
+// }
 
-// Question two
-let questionTwo = validateAnswer('im 23')
-if (questionTwo) {
-    alert('you  are wrong i\'m 24')
-}
+// // Question two
+// let questionTwo = validateAnswer('im 23')
+// if (questionTwo) {
+//     alert('you  are wrong i\'m 24')
+// }
 
-else {
-    alert('you are correct i am not 23')
-    score++;
-}
+// else {
+//     alert('you are correct i am not 23')
+//     score++;
+// }
 
-//Question three
-let questionThree = validateAnswer('I love action movies')
-if (questionThree) {
-    alert('you  are wrong i love mystery')
-}
-else {
-    alert('you are correct!')
-    score++;
-}
+// //Question three
+// let questionThree = validateAnswer('I love action movies')
+// if (questionThree) {
+//     alert('you  are wrong i love mystery')
+// }
+// else {
+//     alert('you are correct!')
+//     score++;
+// }
 
-//Question Four
-let questionFour = validateAnswer('I love walking!')
-if (questionFour) {
-    alert('you  are right what a good sport!')
-    score++;
-}
-else {
-    alert('you are wrong i truly love to walk!')
-}
+// //Question Four
+// let questionFour = validateAnswer('I love walking!')
+// if (questionFour) {
+//     alert('you  are right what a good sport!')
+//     score++;
+// }
+// else {
+//     alert('you are wrong i truly love to walk!')
+// }
 
-//Question Five
-let questionFive = validateAnswer('I know you')
+// //Question Five
+// let questionFive = validateAnswer('I know you')
 
-if (questionFive) {
-    score++
-    alert('you  are right! , i only sent this to people that i know')
-}
-else {
-    alert('you are wrong my friend!')
-}
+// if (questionFive) {
+//     score++
+//     alert('you  are right! , i only sent this to people that i know')
+// }
+// else {
+//     alert('you are wrong my friend!')
+// }
 
-//Question 6 - Guess the num call
+// //Question 6 - Guess the num call
 
 //Question - 7 Guess t=my fav food.
 
